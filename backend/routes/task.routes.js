a8dc365968bb5871cb14629a67137c5bf78edd69
sainRaghuveer <raghuveersain987@gmail.com>
2023-08-03 22:42:13 +0530

    update

--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -3,13 +3,13 @@ const { createTask, updateTask, deleteTask } = require('../controllers/task.cont
 const taskRouter = express.Router();
 
 // ! Pass the board's id in param's of the URL to add task in it
-taskRouter.post("/:id", createTask)
+taskRouter.post("/:id", createTask);
 
 // ! Update specific Task by passing the id of the task in the URL param and the changes in the req-body
-taskRouter.patch('/:id', updateTask)
+taskRouter.patch('/:id', updateTask);
 
 // ! Delete specific Task by passing the id of the task in the URL param and 'boardId' key in req-body
-taskRouter.delete('/:id', deleteTask)
+taskRouter.delete('/:id', deleteTask);
 
 
 
